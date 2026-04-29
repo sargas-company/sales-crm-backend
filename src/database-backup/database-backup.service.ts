3f320d17be9621e77b506240d824ced807a54e00
Dmytro Sarafaniuk <sarafaniuk.dima@gmail.com>
2026-04-29 17:28:15 +0300

    test EVERY_MINUTE

--- src/database-backup/database-backup.service.ts
+++ src/database-backup/database-backup.service.ts
@@ -18,7 +18,7 @@ export class DatabaseBackupService {
     private readonly storage: StorageService,
   ) {}
 
-  @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
+  @Cron(CronExpression.EVERY_MINUTE)
   async runBackup(): Promise<void> {
     this.logger.log('Starting database backup...');
 
